dfb513e37d137fbce55cb69a3592cfa78cbec562
DJ Shin <faizman31@gmail.com>
2023-02-20 12:40:00 +0900

    added cnn_model.py

--- mnist_classification/models/cnn_model.py
+++ mnist_classification/models/cnn_model.py
@@ -24,3 +24,40 @@ class ConvolutionalBlock(nn.Module):
         y = self.layer(x)
         # |y| = (batch_size,out_channel,h,w)
         return y
+
+    
+class ConvolutionalClassifier(nn.Module):
+
+    def __init__(self,output_size):
+        self.output_size=output_size
+
+        self.blocks=nn.Sequential([ # |x| = (n,1,28,28)
+            ConvolutionalBlock(1,32),   # (n,32,14,14)
+            ConvolutionalBlock(32,64),  # (n,64,7,7)
+            ConvolutionalBlock(64,128), # (n,128,4,4)
+            ConvolutionalBlock(128,256),# (n,256,2,2)
+            ConvolutionalBlock(256,512) # (n,512,1,1)
+        ])
+
+        self.fc_layer = nn.Sequential([
+            nn.Linear(512,50),
+            nn.ReLU(),
+            nn.BatchNorm1d(50),
+            nn.Linear(50,output_size),
+            nn.LogSoftmax(dim=-1)
+        ])
+
+    def forward(self,x):
+        assert x.dim() > 2
+
+        if x.dim() == 3: # added channel dim
+            x = x.reshape(x.shape[0],1,x.shape[-2],x.shape[-1])
+
+        # |x| = (batch_size,1,28,28)
+        z = self.blocks(x)
+        # |z| = (batch_size,512,1,1)
+        # |z.squeeze()| = (batch_size,512)
+        y = self.fc_layer(z.squeeze())
+        # |y| = (batch_size,output_size)
+
+        return y
